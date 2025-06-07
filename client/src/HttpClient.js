2c29044d9ee78c699894a965b14d26eb11f86c7c
akwasnik <adrian.kwasnik@gmail.com>
2025-06-07 19:19:53 +0200

    fixed kubernetes bugs

--- client/src/HttpClient.js
+++ client/src/HttpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-   baseURL: 'http://localhost:3050'
+   baseURL: `http://localhost:${process.env.SERVER_PORT || 3050}`,
 });
 
 export {httpClient}
